246e18ca7fb2da47b6138b8bc5d4655c3ce555db
“Harshit <harshitsinghunnao@gmail.com>
2026-02-23 16:04:03 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -24,8 +24,7 @@ app.post(
 
 app.use(cors({
   origin: [
-    "http://localhost:5173",
-    "https://examainotes.netlify.app"
+    "http://localhost:5173"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
