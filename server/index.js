1d0f22156581db3aa062e37d32bae68df3ddbdf5
harshit0414 <144409946+harshit0414@users.noreply.github.com>
2026-03-06 08:25:40 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.post(
 
 app.use(cors({
   origin: [
-    "http://localhost:5173"
+    "https://exam-note-ai-client.onrender.com"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
